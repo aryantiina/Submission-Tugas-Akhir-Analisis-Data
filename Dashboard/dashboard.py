94372758410d7adad1d34959472b0095719b5b60
aryantiina <145460588+aryantiina@users.noreply.github.com>
2026-04-28 16:31:01 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -15,10 +15,9 @@ st.markdown("Dashboard interaktif untuk menjelajahi data pesanan, pengiriman, da
 # --- Load Data ---
 @st.cache_data
 def load_data():
-   base_path = Path(__file__).parent
-    data_path = base_path / "all_data.csv"
-    df = pd.read_csv(data_path, sep=None, engine='python')
-    df.columns = df.columns.str.strip()
+   current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'main_data.csv')
+    df = pd.read_csv('../main_data.csv')
 
     # Convert timestamps
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
